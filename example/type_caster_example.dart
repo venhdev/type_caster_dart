0d368aac27fd0e4922d86277547b787253def709
Venh <venh.dev@gmail.com>
2025-06-24 22:58:15 +0700

    initial package

---
+++ example/type_caster_example.dart
@@ -0,0 +1,6 @@
+import 'package:type_caster/type_caster.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/type_caster_test.dart
@@ -0,0 +1,16 @@
+import 'package:type_caster/type_caster.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
