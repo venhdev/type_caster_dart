cfadefe9f558498c0b249f257c1edbea47adfdc7
Venh <venh.dev@gmail.com>
2025-07-10 23:16:07 +0700

    list ext

---
+++ lib/src/extensions/apis/list_api.dart
@@ -0,0 +1,12 @@
+extension ListApi on List {
+  T? firstWhereOrNull<T>(
+    bool Function(dynamic value) condition, {
+    T Function()? orElse,
+  }) {
+    try {
+      return firstWhere(condition, orElse: orElse);
+    } catch (e) {
+      return null;
+    }
+  }
+}
